0fa55c57950ec15480a2750ec800965bf79bf580
Icakis <Icakis@users.noreply.github.com>
2015-05-25 15:19:03 +0300

    add SingleR + (chat Demo)

---
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿namespace TrafalgarSquare.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class MessagesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
--- TrafalgarSquare/TrafalgarSquare.Web/Startup.cs
+++ TrafalgarSquare/TrafalgarSquare.Web/Startup.cs
@@ -9,6 +9,7 @@ namespace TrafalgarSquare.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
