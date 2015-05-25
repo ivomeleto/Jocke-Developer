2de0d82535a5f95efc44aaa3f330e8f8be995503
Icakis <Icakis@users.noreply.github.com>
2015-05-25 23:45:43 +0300

    Add/Accept/Remove Friend in UsersController

--- TrafalgarSquare/TrafalgarSquare.Web/Controllers/UsersController.cs
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/UsersController.cs
@@ -16,7 +16,6 @@
         {
         }
 
-        // GET: Users
         public ActionResult Index()
         {
             return this.View();
@@ -48,25 +47,73 @@
             // Check if it is already added
             if (userFriend != null)
             {
-                if (userFriend.IsAccepted == true)
+                userFriend.IsAccepted = true;
+                userFriend.SentFriendRequestDate = DateTime.Now;
+            }
+            else
+            {
+                this.Data.UsersFriends.Add(new UserFriends()
                 {
-                    throw new Exception("You are alredy friends.");
-                }
-
-                throw new Exception("Your friend request is waiting acceptance.");
+                    UserId = adderUserId,
+                    FriendId = id,
+                    Friend = friendToAdd,
+                    IsAccepted = true,
+                    SentFriendRequestDate = DateTime.Now
+                });
             }
 
-            this.Data.UsersFriends.Add(new UserFriends()
-            {
-                UserId = adderUserId,
-                FriendId = id,
-                Friend = friendToAdd,
-                IsAccepted = true,
-                SentFriendRequestDate = DateTime.Now
-            });
+            Notification model;
 
-            return this.View();
+            // Send Friend Request.
+            var frinedRequest = this.Data.UsersFriends
+                .All()
+                .FirstOrDefault(x => (x.UserId == friendToAdd.Id && x.FriendId == adderUserId));
+            if (frinedRequest != null)
+            {
+                if (frinedRequest.IsAccepted == false)
+                {
+                    // TODO use SignalR To send notifiacation (to Current User)
+                    model = new Notification()
+                    {
+                        RecepientId = adderUserId,
+                        Text = "Your friend request is waiting acceptance.",
+                        SendDateTime = DateTime.Now
+                    };
+                }
+                else
+                {
+                    // TODO use SignalR To send notifiacation (to Current User)
+                    model = new Notification()
+                    {
+                        RecepientId = adderUserId,
+                        Text = string.Format("You are now friends with {0}", friendToAdd.UserName),
+                        SendDateTime = DateTime.Now
+                    };
+                }
+            }
+            else
+            {
+                this.Data.UsersFriends.Add(new UserFriends()
+                {
+                    UserId = friendToAdd.Id,
+                    FriendId = adderUserId,
+                    IsAccepted = false,
+                    SentFriendRequestDate = DateTime.Now
+                });
+
+                // TODO use SignalR To send notifiacation  (to Current and Friend User)
+                model = new Notification()
+                {
+                    RecepientId = adderUserId,
+                    Text = string.Format("Friend request sent to: {0}", friendToAdd.UserName),
+                    SendDateTime = DateTime.Now
+                };
+            }
+
+            this.Data.SaveChanges();
+            return this.View(model);
         }
+
 
         [Authorize]
         public ActionResult AcceptFriendRequest(string id)
@@ -113,20 +160,44 @@
                 {
                     throw new Exception("Invalid friend request!");
                 }
-
-                this.Data.UsersFriends.Add(new UserFriends()
-                {
-                    UserId = acceptorUserId,
-                    FriendId = newFriend.Id,
-                    Friend = newFriend,
-                    IsAccepted = true,
-                    SentFriendRequestDate = DateTime.Now
-                });
             }
-
 
             this.Data.SaveChanges();
             return this.View();
+        }
+
+        [Authorize]
+        public ActionResult RemoveFriend(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException("id", "Invalid user ID.");
+            }
+
+            var frinedForRemove = this.Data.Users
+                .All()
+                .FirstOrDefault(x => x.Id == id);
+
+            if (frinedForRemove == null)
+            {
+                throw new ArgumentNullException("id", "Invalid user ID.");
+            }
+
+            var userId = User.Identity.GetUserId();
+            var userFriend = this.Data
+                .UsersFriends.All()
+                .FirstOrDefault(x => (x.UserId == userId && x.FriendId == frinedForRemove.Id));
+
+            // Check if they are already friends
+            if (userFriend == null || userFriend.IsAccepted != true)
+            {
+                throw new Exception("You are not friends and cannot remove from friends.");
+            }
+
+            userFriend.IsAccepted = false;
+
+            this.Data.SaveChanges();
+            return new EmptyResult();
         }
     }
 }
