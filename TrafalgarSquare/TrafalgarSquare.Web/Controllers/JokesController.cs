2a84273fd72bfd379aac9e1487130c4c4660ada9
m4r71n85 <martin.tsekov.bg@gmail.com>
2015-05-25 18:03:40 +0300

    update on jokes design, remove data private field

--- TrafalgarSquare/TrafalgarSquare.Web/Controllers/JokesController.cs
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/JokesController.cs
@@ -7,12 +7,10 @@
 
     public class JokesController : BaseController
     {
-        private readonly ITrafalgarSquareData _data;
-
-        // GET: Jockes
+        [Route("jokes")]
         public ActionResult Index()
         {
-            var posts = _data.Posts.All()
+            var posts = Data.Posts.All()
                 .Where(p => p.Category.Name.Equals("Jockes"))
                 .OrderByDescending(p => p.CreatedDateTime)
                 .ThenBy(p => p.LikesPost.Count())
@@ -33,9 +31,30 @@
             return View(posts);
         }
 
+        [HttpGet]
+        [Route("jokes/comments/{postId}")]
+        public ActionResult Comments(int postId)
+        {
+            var comments = Data.Comments.All()
+                .Where(c => c.PostId == postId)
+                .OrderByDescending(c => c.CreatedOn)
+                .Select(c => new CommentViewModel
+                {
+                    Id = c.Id,
+                    Text = c.Text,
+                    CreatedOn = c.CreatedOn,
+                    User = new UserViewModel
+                    {
+                        Id = c.UserId,
+                        Username = c.User.UserName,
+                        AvatarUrl = c.User.AvatarUrl
+                    }
+                });
+            return View(comments);
+        }
+
         public JokesController(ITrafalgarSquareData data) : base(data)
         {
-            _data = data;
         }
     }
 }
