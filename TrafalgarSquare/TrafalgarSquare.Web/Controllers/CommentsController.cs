f8425c703626b260ac24d57521c7f72f3914eed6
ipobornik <gereg.sith@gmail.com>
2015-05-29 06:30:19 +0300

    Working on Joke Controller TODO: Show Comments with AJAX

--- TrafalgarSquare/TrafalgarSquare.Web/Controllers/CommentsController.cs
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/CommentsController.cs
@@ -37,5 +37,27 @@
 
             return this.View(comments);
         }
+
+        public JsonResult DisplayCommentsInPost(int postId)
+        {
+            var comments = Data.Comments.All()
+                .Where(c => c.PostId == postId)
+                .OrderByDescending(c => c.CreatedOn)
+                .Select(c => new CommentViewModel
+                {
+                    Id = c.Id,
+                    CreatedOn = c.CreatedOn,
+                    Text = c.Text,
+                    User = new UserViewModel
+                    {
+                        Id = c.UserId,
+                        Username = c.User.UserName,
+                        AvatarUrl = c.User.AvatarUrl
+                    }
+                })
+                .ToList();
+
+            return this.Json(comments, JsonRequestBehavior.AllowGet);
+        }
     }
 }
--- TrafalgarSquare/TrafalgarSquare.Web/Controllers/JokesController.cs
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/JokesController.cs
@@ -1,4 +1,6 @@
 ﻿
+using TrafalgarSquare.Models;
+
 namespace TrafalgarSquare.Web.Controllers
 {
     using System.Linq;
@@ -21,7 +23,6 @@ namespace TrafalgarSquare.Web.Controllers
             var posts = Data.Posts.All()
                 .Where(p => p.Category.Name.Equals("Jokes"))
                 .OrderByDescending(p => p.CreatedDateTime)
-                .ThenBy(p => p.LikesPost.Count())
                 .Select(p => new JokesViewModel
                 {
                     Id = p.Id,
@@ -41,6 +42,32 @@ namespace TrafalgarSquare.Web.Controllers
                 })
                 .ToList();
             return this.View(posts);
-        }       
+        }
+
+       /* public ActionResult GetPostById(int id)
+        {
+            var posts = Data.Posts.All()
+                .Where(p => p.Category.Name.Equals("Jokes"))
+                .Select(p => new JokesViewModel
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Text = p.Text,
+                    Resource = p.Resource,
+                    CreatedDateTime = p.CreatedDateTime,
+                    IsReported = p.IsReported ?? false,
+                    Likes = p.LikesPost.Count(),
+                    CommentsCount = p.Comments.Count(),
+                    User = new UserViewModel
+                    {
+                        Id = p.PostOwnerId,
+                        Username = p.PostOwner.UserName,
+                        AvatarUrl = p.PostOwner.AvatarUrl
+                    }
+                })
+                .FirstOrDefault(post => post.Id == id);
+
+            return this.View(posts);
+        }    */  
     }
 }
