9528c77786aae53b0802b813127640450d081c98
ipobornik <gereg.sith@gmail.com>
2015-05-31 00:44:06 +0300

    Add-Posts Paging (TODO-handle Index was out of range exeption)

---
+++ TrafalgarSquare/TrafalgarSquare.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrafalgarSquare.Data;
+using TrafalgarSquare.Web.ViewModels;
+
+namespace TrafalgarSquare.Web.Controllers
+{
+    [Authorize]
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(ITrafalgarSquareData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        [Route("Categories/EditorForPosts")]
+        public ActionResult EditorPosts()
+        {
+            return this.View("CreatePostView");
+        }
+
+        [HttpPost]
+        public ActionResult PostCreate(PostCreateBindModel post)
+        {
+            base.BaseForAllCategoriesPostCreat(post);
+
+            return this.Redirect("/");
+        }
+
+        [HttpDelete]
+        [Route("Categories/DeletePost/{postId}")]
+        public ActionResult EditorPosts(int postId)
+        {
+            base.DeletePostInCategorie(postId);
+
+            return this.Redirect("/");
+        }
+
+       
+       
+    }
+}
